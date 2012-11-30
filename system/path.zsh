9ea4f434fd15e1b3c0b0e335015d1bad37c77db6
Stuart Trann <strann@gmail.com>
2012-11-30 10:57:37 -0800

    added java home to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,5 @@
 #export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/share/npm/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export JAVA_HOME="/Library/Java/Home"
+export PATH="$JAVA_HOME/bin:$PATH"
