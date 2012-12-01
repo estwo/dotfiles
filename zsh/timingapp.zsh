2e02ed16f7aed2041c9cdcf48ed23792d962b806
Stuart Trann <strann@gmail.com>
2012-11-30 21:48:56 -0800

    setting for timing.app to track current dir

---
+++ zsh/timingapp.zsh
@@ -0,0 +1,3 @@
+# for timing.app to be able to track the working directory in terminal
+PROMPT_TITLE='echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/~}\007"'
+export PROMPT_COMMAND="${PROMPT_COMMAND} ${PROMPT_TITLE}; "
