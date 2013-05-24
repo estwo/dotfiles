dea31f49d9505543fc08f3d1b7ce0caf346d2eb4
Stuart Trann <strann@gmail.com>
2013-05-24 13:45:12 -0700

    Added script to set tab tible in iTerm2, formatting in prompt.sh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -73,6 +73,7 @@ directory_name(){
 }
 
 export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
